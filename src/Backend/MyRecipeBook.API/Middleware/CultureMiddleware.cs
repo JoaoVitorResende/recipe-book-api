341fcbc113dc66fc6a4fa121efea49d5584a789a
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-02-18 18:39:56 -0300

    feat: Checking if incoming language is on the list to cover the culture

--- src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -5,18 +5,18 @@ namespace MyRecipeBook.API.Middleware
     public class CultureMiddleware
     {
         private readonly RequestDelegate _next;
-        public CultureMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
         public async Task Invoke(HttpContext context)
         {
+            var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
             var requestedCulture = context.Request.Headers["Accept-Language"].FirstOrDefault();
             var cultureCode = requestedCulture.Split(',')[0].Split(';')[0].Trim();
-            var cultureInfo = new CultureInfo(cultureCode);
+            var cultureInfo = new CultureInfo("en");
+            if (!string.IsNullOrWhiteSpace(cultureCode) && supportedLanguages.Any(culture => culture.Name.Equals(cultureCode)))
+                cultureInfo = new CultureInfo(cultureCode);
             CultureInfo.CurrentCulture = cultureInfo;
             CultureInfo.CurrentUICulture = cultureInfo;
             await _next(context);
         }
+        public CultureMiddleware(RequestDelegate next) => _next = next;
     }
 }
