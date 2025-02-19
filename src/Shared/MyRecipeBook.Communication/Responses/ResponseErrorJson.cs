96db96d6226c0d5d9cc0f60a760804040755cd22
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-02-19 18:20:23 -0300

    :feat adição de configuração de filtro de customização de exceções no program e nova classes para gerar os errors de resposta em json e com status code correto

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,9 @@
+﻿namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+        public ResponseErrorJson(IList<string> errors) => Errors = errors;
+        public ResponseErrorJson(string erro) => Errors = [erro];
+    }
+}
--- src/Shared/MyRecipeBook.Exceptions/ExceptionBase/ErrorOnValidationException.cs
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionBase/ErrorOnValidationException.cs
@@ -3,9 +3,6 @@
     public class ErrorOnValidationException : MyRecipeBookException
     {
         public IList<string> ErrorMessages { get; set; }
-        public ErrorOnValidationException(IList<string> errorMessages)
-        {
-            ErrorMessages = errorMessages;
-        }
+        public ErrorOnValidationException(IList<string> errorMessages) => ErrorMessages = errorMessages;
     }
 }
