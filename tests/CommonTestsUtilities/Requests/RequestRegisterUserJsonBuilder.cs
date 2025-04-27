8afffaa10a7809d3fe57676c15ee7bfb43769a56
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-04-27 09:46:25 -0300

    updt: using bogus to insert new data for tests

---
+++ tests/CommonTestsUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bogus;
+using MyRecipeBook.Communication.Requests;
+namespace CommonTestsUtilities.Requests
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build()
+        {
+            return new Faker<RequestRegisterUserJson>()
+            .RuleFor(user => user.Name, (faker) => faker.Person.FirstName)
+            .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+            .RuleFor(user => user.Name, (faker) => faker.Internet.Password());
+        }
+    }
+}
---
+++ tests/Validator.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,16 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+using CommonTestsUtilities.Requests;
+
+namespace Validator.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var result = validator.Validate(request);
+        }
+    }
+}
