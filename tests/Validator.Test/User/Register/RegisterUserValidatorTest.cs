c1cef4cd226d0cbf3bb58917e5d051a2f514fda7
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-05-03 10:05:58 -0300

    feat: validations for email and name, changed: validator for email format

--- tests/Validator.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validator.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,5 +1,6 @@
-﻿using MyRecipeBook.Application.UseCases.User.Register;
-using CommonTestsUtilities.Requests;
+﻿using CommonTestsUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
 
 namespace Validator.Test.User.Register
 {
@@ -12,6 +13,29 @@ namespace Validator.Test.User.Register
             var request = RequestRegisterUserJsonBuilder.Build();
             var result = validator.Validate(request);
             Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ErrorName()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(ResourceMessagesException.NAME_EMPTY, result.Errors.First().ToString());
+        }
+        [Fact]
+        public void ErrorEmail()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = string.Empty;
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(ResourceMessagesException.EMAIL_EMPTY, result.Errors.First().ToString());
         }
     }
 }
