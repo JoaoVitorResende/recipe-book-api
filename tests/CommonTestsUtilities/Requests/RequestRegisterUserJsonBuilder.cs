3cd4e6a80b30a2587b77958e054a77f55131b48c
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-05-03 10:43:28 -0300

    feat: password validation

--- tests/CommonTestsUtilities/Requests/RequestRegisterUserJsonBuilder.cs
+++ tests/CommonTestsUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -4,12 +4,12 @@ namespace CommonTestsUtilities.Requests
 {
     public class RequestRegisterUserJsonBuilder
     {
-        public static RequestRegisterUserJson Build()
+        public static RequestRegisterUserJson Build(int password = 10)
         {
             return new Faker<RequestRegisterUserJson>()
             .RuleFor(user => user.Name, (faker) => faker.Person.FirstName)
             .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
-            .RuleFor(user => user.Password, (faker) => faker.Internet.Password(8, false));
+            .RuleFor(user => user.Password, (faker) => faker.Internet.Password(password));
         }
     }
 }
--- tests/Validator.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validator.Test/User/Register/RegisterUserValidatorTest.cs
@@ -14,7 +14,6 @@ namespace Validator.Test.User.Register
             var result = validator.Validate(request);
             Assert.True(result.IsValid);
         }
-
         [Fact]
         public void ErrorName()
         {
@@ -37,5 +36,32 @@ namespace Validator.Test.User.Register
             Assert.Single(result.Errors);
             Assert.Equal(ResourceMessagesException.EMAIL_EMPTY, result.Errors.First().ToString());
         }
+        [Fact]
+        public void ErrorEmailInvalid()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = "email.com";
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(ResourceMessagesException.EMAIL_WRONG_FORMAT, result.Errors.First().ToString());
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void ErrorPassword(int passwordLenght)
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build(passwordLenght);
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(ResourceMessagesException.PASSWORD_NOT_GREAT_THEN_SIX_CHARACTERS, result.Errors.First().ToString());
+        }
+
     }
 }
