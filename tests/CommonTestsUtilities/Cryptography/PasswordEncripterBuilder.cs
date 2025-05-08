d0a07ae08e4dc0ab9d539275545d3971964726fc
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-05-08 19:27:26 -0300

    feat: new tests RegisterUserUseCase

---
+++ tests/CommonTestsUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,8 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+namespace CommonTestsUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        public static PasswordEncryption Build() => new PasswordEncryption("abc1234");
+    }
+}
---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CommonTestsUtilities.Cryptography;
+using CommonTestsUtilities.Mapper;
+using CommonTestsUtilities.Repositories;
+using CommonTestsUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace UseCases.Test.User.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var mapper = MapperBuilder.Build();
+            var encripter = PasswordEncripterBuilder.Build();
+            var WritteRepository = UserWriteOnlyRepositoryBuilder.Build();
+            var unityOfWork = UnityOfWorkBuilder.Build();
+            var useCase = new RegisterUserUseCase(encripter,mapper);
+            var result = await useCase.Execute(request);
+            Assert.NotNull(result);
+            Assert.Equal(result.Name, request.Name);
+        }
+    }
+}
