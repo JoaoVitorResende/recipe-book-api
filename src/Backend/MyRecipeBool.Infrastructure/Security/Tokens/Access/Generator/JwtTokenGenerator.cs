21afe34a08c4978a7e77ce04b2f8c021576cef09
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-06-17 17:55:19 -0300

    feat: creating jwt token and interface

---
+++ src/Backend/MyRecipeBool.Infrastructure/Security/Tokens/Access/Generator/JwtTokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using MyRecipeBook.Domain.Security.Tokens;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens.Access.Generator
+{
+    internal class JwtTokenGenerator : IAccessTokenGenerator
+    {
+        private readonly uint _expirationTimeMinutes;
+        private readonly string _singingKey;
+        public JwtTokenGenerator(uint expirationTimeMinutes, string singingKey)
+        {
+            _expirationTimeMinutes = expirationTimeMinutes;
+            _singingKey = singingKey;
+        }
+        public string Generate(Guid userIdentifier)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Sid, userIdentifier.ToString())
+            };
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+                SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
+        }
+        private SymmetricSecurityKey SecurityKey() 
+        {
+           return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_singingKey));
+        }
+    }
+}
