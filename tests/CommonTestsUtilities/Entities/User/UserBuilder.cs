5cfcf28f6c361061d69290623493af737e438390
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-06-28 14:04:15 -0300

    feat: loggedUseCaseTest

--- tests/CommonTestsUtilities/Entities/User/UserBuilder.cs
+++ tests/CommonTestsUtilities/Entities/User/UserBuilder.cs
@@ -4,7 +4,6 @@ namespace CommonTestsUtilities.Entities.User
 {
     public class UserBuilder
     {
-        
         public static (MyRecipeBook.Domain.Entities.User user, string password) Build()
         {
             var passwordEncripter = PasswordEncripterBuilder.Build();
@@ -13,6 +12,7 @@ namespace CommonTestsUtilities.Entities.User
                 .RuleFor(user => user.Id, () => 1)
                 .RuleFor(user => user.Name, (f) => f.Person.FirstName)
                 .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.UserIdentifier, _ => Guid.NewGuid())
                 .RuleFor(user => user.Password, (f) => passwordEncripter.Encript(password));
             return (user, password);
         }
