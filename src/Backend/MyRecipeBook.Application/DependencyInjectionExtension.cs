1d88db4576d247635d3f0c93f63d052facbac8a1
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-07-28 20:57:49 -0300

    feat: starting post recipe filters to search an recipe

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Application.Services.AutoMapper;
 using MyRecipeBook.Application.UseCases.Login.DoLogin;
+using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Application.UseCases.User.ChangePassword;
 using MyRecipeBook.Application.UseCases.User.Profile;
@@ -38,6 +39,7 @@ namespace MyRecipeBook.Application
             services.AddScoped<IGetUserProfileUseCase, GetUserProfileUseCase>();
             services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
             services.AddScoped<IChangePasswordUseCase, ChangePasswordUseCase>();
+            services.AddScoped<IFilterRecipeUseCase, FilterRecipeUseCase>();
         }
     }
 }
---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Filter
+{
+    public class FilterRecipeUseCase : IFilterRecipeUseCase
+    {
+        private readonly IMapper _mapper;
+        private readonly IloggedUser _loggedUser;
+        public FilterRecipeUseCase(IMapper mapper, IloggedUser loggedUser)
+        {
+            _mapper = mapper;
+            _loggedUser = loggedUser;
+        }
+        public async Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request)
+        {
+            Validate(request);
+            var loggedUser = await _loggedUser.User();
+            return new ResponseRecipesJson 
+            {
+                Recipes = []
+            };
+        }
+        private static void Validate(RequestFilterRecipeJson request)
+        {
+            var validator = new FilterValidator();
+            var result = validator.Validate(request);
+            if(!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).Distinct().ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+}
