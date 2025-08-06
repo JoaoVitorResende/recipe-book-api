6da3222f285f92abaf98f12193aab9c6f4897021
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-08-06 19:53:53 -0300

    feat : creating unit tests, use case and validation

---
+++ tests/CommonTestsUtilities/Requests/RequestFilterRecipeJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Enuns;
+
+namespace CommonTestsUtilities.Requests
+{
+    public class RequestFilterRecipeJsonBuilder
+    {
+        public static RequestFilterRecipeJson Build()
+        {
+            return new Faker<RequestFilterRecipeJson>()
+                .RuleFor(user => user.RecipeTitle_Ingredient, faker => faker.Lorem.Word())
+                .RuleFor(user => user.CookingTimes, (faker) => faker.Make(1,() => faker.PickRandom<CookingTime>()))
+                .RuleFor(user => user.Difficulties, (faker) => faker.Make(1, () => faker.PickRandom<Difficulty>()))
+                .RuleFor(user => user.DishTypes, (faker) => faker.Make(1, () => faker.PickRandom<DishType>()));
+        }
+    }
+}
